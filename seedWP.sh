3e0912923722980bdc630ad3f2a0db7b0fe41cca
Mike iLL Kilmer <mike@mzoo.org>
2018-02-11 13:08:01 -0600

    Also the contact form seven plugins we want.

--- seedWP.sh
+++ seedWP.sh
@@ -7,4 +7,7 @@ docker-compose exec --user www-data wordpress wp post create --post_type=page --
 docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Careers" --post_status=published
 docker-compose exec --user www-data wordpress wp menu create "Primary Navigation"
 docker-compose exec --user www-data wordpress wp theme activate phyt-theme
-docker-compose exec --user www-data wordpress wp plugin install simple-locator --activate
+docker-compose exec --user www-data wordpress wp plugin install advanced-custom-fields simple-locator --activate
+docker-compose exec --user www-data wordpress wp plugin activate phyt-jobs
+docker-compose exec --user www-data wordpress wp plugin install contact-form-7 --activate
+docker-compose exec --user www-data wordpress wp plugin install contact-form-7-city-field-extension contact-form-7-dynamic-text-extension --activate
