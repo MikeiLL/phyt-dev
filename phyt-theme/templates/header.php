041ca011c7d976edefe210505bca5dd37ca2d537
Mike iLL Kilmer <mike@mzoo.org>
2018-02-11 16:34:24 -0600

    A bootstrap 4 responsive nav.

--- phyt-theme/templates/header.php
+++ phyt-theme/templates/header.php
@@ -1,12 +1,16 @@
-<header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
+<nav class="navbar navbar-expand-lg sticky-top navbar-light bg-light">
+  <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img alt="<?php bloginfo('name'); ?> logo" src="<?= get_template_directory_uri(); ?>/dist/images/phyt-logo.png"><span class="sr-only">Home</span></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
-    </nav>
   </div>
-</header>
+</nav>
+
+
+
