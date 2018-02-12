2b655719acae8a51fa1fea24e2ae6af24baec7f1
Mike iLL Kilmer <mike@mzoo.org>
2018-02-11 23:22:27 -0600

    Add variable for font load script.

--- phyt-theme/templates/head.php
+++ phyt-theme/templates/head.php
@@ -3,4 +3,8 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <script type="text/javascript">
+  /* Create path variable for our custom font load script */
+  var customPath = '<?= get_bloginfo("template_url"); ?>/dist/fonts';
+  </script>
 </head>
