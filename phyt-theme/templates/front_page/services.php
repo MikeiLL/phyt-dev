29abfb7c336ff645019c06115faef9ba19d746ab
Mike iLL Kilmer <mike@mzoo.org>
2018-02-12 04:54:07 -0600

    Correct seed wp post calls and update services copy.

--- phyt-theme/templates/front_page/services.php
+++ phyt-theme/templates/front_page/services.php
@@ -2,7 +2,7 @@
   <div>
   <h2 class="text-center">Our Services</h2>
     <p>
-    A national rehabilitation company, Phyt provides services to patients in skilled nursing facilities, outpatient, and community programs. Our therapists, the #PhytSquad are a national community of licensed, experienced, results-driven natural caregivers.
+    A national rehabilitation company, Phyt provides services to patients in skilled nursing &amp; assisted living facilities as well as outpatient programs. Our therapists, the #PhytSquad are a national community of licensed, experienced, results-driven natural caregivers.
     <p>
   </div>
   <div class="row">
@@ -10,13 +10,13 @@
       <h3>Skilled Nursing &amp; Assisted Living Facilities</h3>
       <p>
       lorem ipsum dolor etc...
-      <p>
+      </p>
     </div>
     <div class="col-12 col-md-6">
       <h3>Outpatient Programs</h3>
       <p>
       lorem ipsum dolor etc...
-      <p>
+      </p>
     </div>
   </div>
 </div>
--- seedWP.sh
+++ seedWP.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_title="Front Page" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="About Us" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Patients" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Partners" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Careers" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=job --post_content --post_title="Manhattan Manager" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=job --post_content --post_title="Brooklyn Manager" --post_status=published
-docker-compose exec --user www-data wordpress wp post create --post_type=job --post_content --post_title="Queens Manager" --post_status=published
+docker-compose exec --user www-data wordpress wp post create --post_type=page --post_title="Front Page" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="About Us" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Patients" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Partners" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Careers" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=jobs --post_content --post_title="Manhattan Manager" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=jobs --post_content --post_title="Brooklyn Manager" --post_status=publish
+docker-compose exec --user www-data wordpress wp post create --post_type=jobs --post_content --post_title="Queens Manager" --post_status=publish
 docker-compose exec --user www-data wordpress wp menu create "Primary Navigation"
 docker-compose exec --user www-data wordpress wp theme activate phyt-theme
 docker-compose exec --user www-data wordpress wp plugin install advanced-custom-fields simple-locator --activate
