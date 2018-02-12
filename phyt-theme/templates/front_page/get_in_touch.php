b9de921ad63a2b1f3e8283e65e5004470e57227b
Mike iLL Kilmer <mike@mzoo.org>
2018-02-12 00:44:35 -0600

    Add background image.

--- phyt-theme/templates/front_page/get_in_touch.php
+++ phyt-theme/templates/front_page/get_in_touch.php
@@ -1,11 +1,11 @@
-<section class="contact-frontpage">
+<section class="contact-frontpage ">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-12">
-        <h3 class="text-center">Get In Touch</h3>
+      <div class="col-12 text-center text-light">
+        <h3 class="">Get In Touch</h3>
         <hr />
         Learn more about bla bla bla
-        <div>
+        <div class="mx-auto">
           <?php get_template_part('templates/content', 'page'); ?>
         </div>
       </div>
