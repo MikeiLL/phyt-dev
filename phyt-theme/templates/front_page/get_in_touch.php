79a9002e688c7084bd2d1c3790f79112456f7714
toptomato <tom@lexwebdev.com>
2018-02-12 16:21:16 -0500

    first commit w/ new theme. Sass mods and also front page tpl files

--- phyt-theme/templates/front_page/get_in_touch.php
+++ phyt-theme/templates/front_page/get_in_touch.php
@@ -1,9 +1,9 @@
-<section class="contact-frontpage">
+<section class="frontpage-contact frontpage-section home-contact">
   <div class="container-fluid py-2">
     <div class="row">
       <div class="col-12 text-center text-light">
-        <h3 class="">Get In Touch</h3>
-        <hr />
+        <h2 class="frontpage-contact__title">Get In Touch</h2>
+        <hr class="front-page__hr frontpage-contact__hr" />
         Learn more about bla bla bla
         <div class="mx-auto">
             <?php get_template_part('templates/content', 'page'); ?>
--- phyt-theme/templates/front_page/services.php
+++ phyt-theme/templates/front_page/services.php
@@ -1,26 +1,26 @@
-<div class="container py-2">
-  <div>
-  <h2 class="text-center">Our Services</h2>
+<section class="frontpage-services frontpage-section container py-2">
+  <div class="frontpage-services__wrapper">
+  <h2 class="frontpage-services__title">Our Services</h2>
     <p>
     A national rehabilitation company, Phyt provides services to patients in skilled nursing &amp; assisted living facilities as well as outpatient programs. Our therapists, the #PhytSquad are a national community of licensed, experienced, results-driven natural caregivers.
     <p>
   </div>
-  <div class="row">
+  <div class="row frontpage-services__service">
     <div class="col-12 col-md-6">
-      <h3>Skilled Nursing &amp; Assisted Living Facilities</h3>
+      <h3 class="frontpage__header">Skilled Nursing &amp; Assisted Living Facilities</h3>
       <p>
       lorem ipsum dolor etc...
       </p>
     </div>
     <div class="col-12 col-md-6">
-      <h3>Outpatient Programs</h3>
+      <h3 class="frontpage__header">Outpatient Programs</h3>
       <p>
       lorem ipsum dolor etc...
       </p>
     </div>
   </div>
-</div>
+</section>
 
 
 
-.
+
