4797a4deabfeed29d5dbf79f3d07353eb1699fc6
toptomato <tom@lexwebdev.com>
2018-02-12 20:34:04 -0500

    sass and front page tpl changes. this commit prior to first bash upload

--- phyt-theme/templates/front_page/get_in_touch.php
+++ phyt-theme/templates/front_page/get_in_touch.php
@@ -2,11 +2,14 @@
   <div class="container-fluid py-2">
     <div class="row">
       <div class="col-12 text-center text-light">
-        <h2 class="frontpage-contact__title">Get In Touch</h2>
-        <hr class="front-page__hr frontpage-contact__hr" />
-        Learn more about bla bla bla
-        <div class="mx-auto">
-            <?php get_template_part('templates/content', 'page'); ?>
+        <div class="frontpage-contact__form-wrapper">
+          <h2 class="frontpage-contact__title">Get In Touch</h2>
+          <hr class="front-page__hr frontpage-contact__hr" />
+          <p class="frontpage-contact__text">Learn more about our services, employment opportunities, and B2B solutions
+          <div class="mx-auto">
+              <?php get_template_part('templates/content', 'page'); ?>
+              
+          </div>
         </div>
       </div>
     </div>
