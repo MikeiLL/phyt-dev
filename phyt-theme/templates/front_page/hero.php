b6269e9473e1aa9c0c9d237510994970809469c9
toptomato <tom@lexwebdev.com>
2018-02-13 11:15:35 -0500

    SASS changes and hero changes

--- phyt-theme/templates/front_page/hero.php
+++ phyt-theme/templates/front_page/hero.php
@@ -13,7 +13,7 @@
         <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
           <div class="hero__card card text-white bg-info mb-3 text-center">
             <div class="card-block">
-              <p class="card-text"><span class="glyphicon glyphicon-time"></span>Call Us</p>
+              <p class="card-text"><span class="glyphicon glyphicon-time"></span><span class="oi" data-glyph="clock"></span>Call Us</p>
               <hr />
               <p class="card-text"><a href="tel:+8885753483">Toll Free: 1.888.575.3483</a></p>
               <hr />
