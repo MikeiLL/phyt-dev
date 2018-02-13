9b945e8c2d2ea97dd0ac3938f6eea994ad9433d6
Mike iLL Kilmer <mike@mzoo.org>
2018-02-13 04:39:08 -0600

    Stylize and add link to below on page.

--- phyt-theme/templates/careers/hero.php
+++ phyt-theme/templates/careers/hero.php
@@ -1,8 +1,11 @@
-<?php
-
-?>
 <section class="hero hero-careers">
   <div class="container-fluid">
-    <h1>Join the #PhytSquad</h1>
+    <div class="row justify-content-center">
+      <h1 class="text-white text-center" style="width:100%">Join the #PhytSquad</h1>
+      <div>
+        <a id="jumpdown" class="text-white" href="#blade-two">click me</a>
+      </div>
+    </div>
   </div>
 </section>
+<span id="blade-two"></span>
