86ecc71af347e38e410ac5491e126f919ce9a708
Mike iLL Kilmer <mike@mzoo.org>
2018-02-13 14:39:00 -0600

    Add some custom icons via inline svg with file_get_contents.

--- phyt-theme/templates/footer.php
+++ phyt-theme/templates/footer.php
@@ -16,6 +16,68 @@
       </div>
 
       <div class="col-12 col-md-6 clearfix">
+        <div class="clearfix mhl ptl">
+                <h1 class="mvm mtn fgc1">Grid Size: 16</h1>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg><span class="name"> icon-phone</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-location"><use xlink:href="#icon-location"></use></svg><span class="name"> icon-location</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-location2"><use xlink:href="#icon-location2"></use></svg><span class="name"> icon-location2</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-info"><use xlink:href="#icon-info"></use></svg><span class="name"> icon-info</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-facebook"><use xlink:href="#icon-facebook"></use></svg><span class="name"> icon-facebook</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-facebook2"><use xlink:href="#icon-facebook2"></use></svg><span class="name"> icon-facebook2</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-twitter"><use xlink:href="#icon-twitter"></use></svg><span class="name"> icon-twitter</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-linkedin"><use xlink:href="#icon-linkedin"></use></svg><span class="name"> icon-linkedin</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-linkedin2"><use xlink:href="#icon-linkedin2"></use></svg><span class="name"> icon-linkedin2</span>
+                    </div>
+                </div>
+                <div class="glyph fs1">
+                    <div class="clearfix pbs">
+                        <svg class="icon icon-clock-o"><use xlink:href="#icon-clock"></use></svg><span class="name"> icon-clock</span>
+                    </div>
+                </div>
+          </div>
+
+          <div class="glyph fs1">
+              <div class="clearfix pbs">
+                  <svg class="icon hero-caret"><use xlink:href="#icon-downcaret"></use></svg><span class="name"> icon-downcaret</span>
+              </div>
+          </div>
+      </div>
+
+      <div class="col-12 col-md-6 clearfix">
         <div class="float-right text-right">America's leading physical,<br /> occupational and speech <br/>therapy group. <p>#PhytSquad</p>SOCIAL LINKS</div>
       </div>
 
