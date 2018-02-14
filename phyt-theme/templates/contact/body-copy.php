e5b42e62fa7e897ca1846fdc0b1ad0fddbf51812
Mike iLL Kilmer <mike@mzoo.org>
2018-02-14 00:12:03 -0600

    Working on the Contact page and Reveal styling.

---
+++ phyt-theme/templates/contact/body-copy.php
@@ -0,0 +1,61 @@
+<div class="container-fluid contact-page" itemscope itemtype="http://schema.org/MedicalOrganization">
+  <div class="row border border-light">
+    <div class="col-12 col-sm-8">
+      <h6 class="card-subtitle" itemprop="name">Phyt Rehab</h6>
+      We understand the right fit makes all the difference
+      We’re available to answer your questions, meet your needs, and rapidly deploy our services. Reach out!
+
+    </div>
+
+    <div class="col-12 col-sm-4 bg-light" >
+      <h2>GENERAL INQUIRIES</h2>
+
+      <h3 itemprop="phone">1.888.575.3483</h3>
+
+      <div class="address" itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+        <span itemprop="streetAddress">171 Kings Highway</span><br />
+        <span itemprop="addressLocality">Brooklyn</span>,
+        <span itemprop="addressRegion">NY</span>
+        <span itemprop="postalCode">11223</span><br />
+        <span itemprop="email">info@phytrehab.com</span>
+      </div>
+
+
+      DEPARTMENTS
+      <ul id="reveal">
+        <li class="" role="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne"><svg class="icon icon-plus"><use xlink:href="#icon-plus"></use></svg> Business Inquiries</li>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#reveal">
+          Daisy Stephen, Vice President
+          Phone: 914.837.6538
+          Email: daisy@phytrehab.com
+        </div>
+        <li class="" role="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo"><svg class="icon icon-plus"><use xlink:href="#icon-plus"></use></svg> Recruitment</li>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingOne" data-parent="#reveal">
+          Mercenda Boyd
+          Phone: (718) 434-5100 ext.2320
+          Fax: (718) 872-1810
+          Email: mboyd@phytrehab.com
+        </div>
+
+        <li class="" role="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree"><svg class="icon icon-plus"><use xlink:href="#icon-plus"></use></svg> Human Resources</li>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingOne" data-parent="#reveal">
+          Michelle Falotico
+          Phone: 718-434-5100 extension 2326
+          Email: mfalotico@phytrehab.com
+        </div>
+
+        <li class="" role="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour"><svg class="icon icon-plus"><use xlink:href="#icon-plus"></use></svg> Corporate Office</li>
+        <div id="collapseFour" class="collapse" aria-labelledby="headingOne" data-parent="#reveal">
+            Phyt Rehab
+            171 Kings Highway
+            Brooklyn, NY 11223
+            Toll Free: 1.888.575.3483
+        </div>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<h3 class="">Contact us today to learn more about how our services can work for you.</h3>
+
+
