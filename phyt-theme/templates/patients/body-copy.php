11a292ff1c1f8dc9ef799679869e6a9490448bbb
toptomato <tom@lexwebdev.com>
2018-02-15 02:06:28 -0500

    css and tpl changes throughout. add new color vars

--- phyt-theme/templates/patients/body-copy.php
+++ phyt-theme/templates/patients/body-copy.php
@@ -1,16 +1,18 @@
 <section class="patients-main">
 <div class="container">
   <div class="row border border-light">
-    <div class="col-8">
-      <blockquote>Wanting to go home. Yearning to be back to your old self, able to do all the things you used to do on your own. We know.</blockquote>
+    <div class="col-md-8 col-sm-12">
+      <blockquote class="patients__blockquote">Wanting to go home. Yearning to be back to your old self, able to do all the things you used to do on your own. We know.</blockquote>
     </div>
-    <div class="col-8">
+  </div>
+    <div class="row">
+    <div class="col-md-8 col-sm-12">
       <p>When you’re well enough to qualify as a patient in an outstanding rehabilitation center–the quality of facilities that Phyt partners with–you are not just taken care of, you are cared for. Our experienced, informed therapists, the #PhytSquad, work patiently to help clients regain facilities to their maximum potential.</p>
 
       <p>Over more than three decades, Phyt have partnered with hundreds of organizations and helped thousands of patients face challenges in the following three fields:</p>
     </div>
 
-    <div class="col-4">
+    <div class="col-md-4 col-sm-12">
       <img src="<?= get_template_directory_uri(); ?>/dist/images/patients-icon.png" class="img-fluid" alt="Phyt Icon with Inlaid Photos" />
     </div>
   </div>
@@ -21,17 +23,17 @@
     <div class="row border border-light">
     
     <div class="col-12 col-md-4">
-      <h3>Physical Therapy</h3>
+      <h3 class="patients-therapy__title">Physical Therapy</h3>
       <p>Your Physical Therapist will work with you to create an individualized program towards getting you back home as soon as possible. After doing an initial evaluation, the Physical Therapy team will create a plan of care established to improve your mobility, balance, strength, and safety.</p>
     </div>
     
     <div class="col-12 col-md-4">
-      <h3>Occupational Therapy</h3>
+      <h3 class="patients-therapy__title">Occupational Therapy</h3>
       <p>Your Occupational Therapist will assist you to re-develop the skills you need to function and live life to its fullest. After your initial evaluation, the Occupational Therapy team will help you strengthen the skills to complete your activities of daily living and what you need to be able to do to return to your unique home environment.</p>
     </div>
     
     <div class="col-12 col-md-4">
-      <h3>Speech Therapy</h3>
+      <h3 class="patients-therapy__title">Speech Therapy</h3>
       <p>Speech Language Pathologists (SLPs) are trained in areas of oral motor skills such as swallowing, cognitive communication and orientation. Your SLP will collaborate with you and the Interdisciplinary team to improve your cognition, communication, and swallowing function.</p>
     </div>
     <blockquote class="pullquote">Contact us today to learn more about how our services can work for you.</blockquote>
