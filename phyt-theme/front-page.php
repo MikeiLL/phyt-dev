dd6d7a2b4a868b69236c49ee4983af8d1078b969
Mike iLL Kilmer <mike@mzoo.org>
2018-02-15 01:25:52 -0600

    Add the nodes image to the front page.

--- phyt-theme/front-page.php
+++ phyt-theme/front-page.php
@@ -2,5 +2,6 @@
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/front_page/hero'); ?>
   <?php get_template_part('templates/front_page/services'); ?>
+  <?php get_template_part('templates/front_page/nodes'); ?>
   <?php get_template_part('templates/front_page/get_in_touch'); ?>
 <?php endwhile; ?>
---
+++ phyt-theme/templates/front_page/nodes.php
@@ -0,0 +1,7 @@
+<section class="frontpage-nodes container-fluid">
+  <img alt="Interconnected Nodes" src="<?= get_template_directory_uri(); ?>/dist/images/large-nodes.png">
+</section>
+
+
+
+
