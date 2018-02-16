830eb9c2ac001cbd4a90e2a4b284633aa6f5d70d
Mike iLL Kilmer <mike@mzoo.org>
2018-02-15 23:34:21 -0600

    Return to universal responsive typography.

--- phyt-theme/templates/front_page/services.php
+++ phyt-theme/templates/front_page/services.php
@@ -1,11 +1,11 @@
-<section class="frontpage-services frontpage-section container-fluid py-2">
+<section class="container-fluid main">
   <div class="frontpage-services__wrapper">
-  <h2 class="frontpage-services__title">Our Services</h2>
+  <h2>Our Services</h2>
     <p>
     A national rehabilitation company, Phyt provides services to patients in skilled nursing &amp; assisted living facilities as well as outpatient programs. Our therapists, the #PhytSquad are a national community of licensed, experienced, results-driven natural caregivers.
     <p>
   </div>
-  <div class="row frontpage-services__service">
+  <div class="row">
     <div class="col-12 col-md-6">
       <h3 class="frontpage__header">Skilled Nursing &amp; Assisted Living Facilities</h3>
       <p>There's no substitute for proximity to skilled management and therapists, which is exactly what happens at the residential facilities in which we and our community of caregivers operate.</p>
