369db03e76adbf859b51578178e317860370a092
toptomato <tom@lexwebdev.com>
2018-02-16 16:16:34 -0500

    frontpage tweaks

--- phyt-theme/templates/front_page/services.php
+++ phyt-theme/templates/front_page/services.php
@@ -1,4 +1,4 @@
-<section class="container-fluid main">
+<section class="container main">
   <div class="frontpage-services__wrapper">
   <h2>Our Services</h2>
     <p>
@@ -6,11 +6,11 @@
     <p>
   </div>
   <div class="row">
-    <div class="col-12 col-md-6">
+    <div class="col-12 col-md-6 frontpage-services__service">
       <h3 class="frontpage__header">Skilled Nursing &amp; Assisted Living Facilities</h3>
       <p>There's no substitute for proximity to skilled management and therapists, which is exactly what happens at the residential facilities in which we and our community of caregivers operate.</p>
     </div>
-    <div class="col-12 col-md-6">
+    <div class="col-12 col-md-6 frontpage-services__service">
       <h3 class="frontpage__header">Outpatient Programs</h3>
       <p>Patients who either don't qualify for or don't require a residential rehabilitation program can make significant progress through consistent outpatient work, and of course diligent practice at home.</p>
     </div>
