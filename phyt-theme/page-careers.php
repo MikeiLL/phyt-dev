3743090f6355beee5a3a7fc1003c4b850020c649
Mike iLL Kilmer <mike@mzoo.org>
2018-02-15 23:52:25 -0600

    Wrap in containers.

--- phyt-theme/page-careers.php
+++ phyt-theme/page-careers.php
@@ -1,5 +1,14 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/careers/hero'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
+<section class="partners__content">
+  <div class="container ">
+      <div class="row">
+        <div class="col-12">
+         <?php get_template_part('templates/content', 'page'); ?>
+        </div>
+    </div>
+  </div>
+</section>
+
 <?php endwhile; ?>
