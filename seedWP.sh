7cead616bfd3caa56b4b9e79ae332db5202950ab
Mike iLL Kilmer <mike@mzoo.org>
2018-07-30 12:17:25 -0500

    Update seed to remove unneeded elements.

--- seedWP.sh
+++ seedWP.sh
@@ -1,17 +1,14 @@
 #!/usr/bin/env bash
 
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_title="Front Page" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="About Us" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Patients" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Partners" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Careers" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=page --post_content --post_title="Contact" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=jobs --post_content --post_title="Manhattan Manager" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=jobs --post_content --post_title="Brooklyn Manager" --post_status=publish
-docker-compose exec --user www-data wordpress wp post create --post_type=jobs --post_content --post_title="Queens Manager" --post_status=publish
-docker-compose exec --user www-data wordpress wp menu create "Primary Navigation"
-docker-compose exec --user www-data wordpress wp theme activate phyt-theme
-docker-compose exec --user www-data wordpress wp plugin install advanced-custom-fields simple-locator --activate
-docker-compose exec --user www-data wordpress wp plugin activate phyt-jobs
-docker-compose exec --user www-data wordpress wp plugin install contact-form-7 --activate
-docker-compose exec --user www-data wordpress wp plugin install contact-form-7-city-field-extension contact-form-7-dynamic-text-extension --activate
+docker-compose run --user www-data --rm wp-cli wp post create --post_type=page --post_title="Front Page" --post_status=publish
+docker-compose run --user www-data --rm wp-cli wp post create --post_type=page --post_content="" --post_title="About Us" --post_status=publish
+docker-compose run --user www-data --rm wp-cli wp post create --post_type=page --post_content="" --post_title="Patients" --post_status=publish
+docker-compose run --user www-data --rm wp-cli wp post create --post_type=page --post_content="" --post_title="Partners" --post_status=publish
+docker-compose run --user www-data --rm wp-cli wp post create --post_type=page --post_content="" --post_title="Careers" --post_status=publish
+docker-compose run --user www-data --rm wp-cli wp post create --post_type=page --post_content="" --post_title="Contact" --post_status=publish
+docker-compose run --user www-data --rm wp-cli wp menu create "Primary Navigation"
+docker-compose run --user www-data --rm wp-cli wp theme activate phyt-theme
+docker-compose run --user www-data --rm wp-cli wp plugin install advanced-custom-fields simple-locator --activate
+docker-compose run --user www-data --rm wp-cli wp plugin activate phyt-jobs
+docker-compose run --user www-data --rm wp-cli wp plugin install contact-form-7 --activate
+docker-compose run --user www-data --rm wp-cli wp plugin install contact-form-7-city-field-extension contact-form-7-dynamic-text-extension --activate
