85c7886f296884b89da0134d2dc1af4852ba9db2
Mike iLL Kilmer <mike@mzoo.org>
2018-07-30 11:36:54 -0500

    Remove heading from careers page.

--- phyt-theme/page-careers.php
+++ phyt-theme/page-careers.php
@@ -5,10 +5,6 @@
   <div class="container ">
       <div class="row">
         <div class="col-12">
-          <section class="careers__content">
-            <h2>Phyt Rehab is always looking for capable, qualified and compassionate people to join our team. Use the search function below to find openings by geographic area.</h2>
-            Our recruitment team is always accepting resumes and can help identify positions for qualified individuals. Please submit your contact information below.
-          </section>
          <?php get_template_part('templates/content', 'page'); ?>
         </div>
     </div>
